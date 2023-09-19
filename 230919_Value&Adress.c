be0d9cf766f98c745e4142e86fab1c02cec9f96b
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-09-19 15:13:35 +0900

    Create 230919_Value&Adress.c

---
+++ 230919_Value&Adress.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int test_func() {
+	int data = 5;
+	int* ptr = &data;
+
+	printf("%d\n", data);		// data의 저장된 값						: 5
+	printf("%p\n", &data);		// data의 주소							: 0096F9B4
+	printf("%d\n", *(&data));	// data의 주소에 담겨져 있는 값			: 5
+	printf("%p\n", ptr);		// data의 주소를 담고 있는 ptr의 값		: 0096F9B4
+	printf("%d\n", *ptr);		// ptr이 가리키는 주소에 담겨져 있는 값 : 5
+	printf("%p\n", &ptr);		// 포인터 변수 ptr의 주소				: 0096F9A8
+}
+
+int main() {
+	test_func();
+
+	return 0;
+}
