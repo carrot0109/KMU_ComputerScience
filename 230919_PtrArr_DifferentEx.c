b17ccdcaa6e5f764a5524d3682fc06f484019996
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-09-19 15:46:30 +0900

    Create 230919_PtrArr_DifferentEx.c

---
+++ 230919_PtrArr_DifferentEx.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int test_func() {
+	int arr[5] = { 10,20,30,40,50 };
+	int* ap = arr;
+
+	int i = 0;
+
+	for (i = 0; i < 5; i++) {
+		printf("*(arr + i) : %d		*arr + i : %d\n", *(arr + i), *arr + i);      // 첫 째 코드 : 주소 자체를 연산    둘 째 코드 : 주소를 통해 값을 불러드린 후 연산
+	}
+}
+
+int main() {
+	test_func();
+
+	return 0;
+}
