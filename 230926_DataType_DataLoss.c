807ee0a448c305285fbf7b9c73d757bdcd9e95b7
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-09-26 16:22:56 +0900

    Create 230926_DataType_DataLoss.c

---
+++ 230926_DataType_DataLoss.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void test_func1() {
+	int a = 0;
+	double b = 0;
+
+	a = 3.2;    // 자료 손실
+	b = 3.2;
+
+	printf("int 자료형 : %d\n", a);
+	printf("double 자료형 : %lf\n", b);
+}
+
+void test_func2() {
+	int a = 0;
+	double b = 3.2;
+
+	a = (int)b;
+	printf("int 자료형 : %d\n", a);
+}
+
+int main() {
+	test_func1();
+	test_func2();
+
+	return 0;
+}
