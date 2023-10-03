eb3a54c7d999f0829f9b1a330cc1be618305adcd
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-03 22:28:15 +0900

    Update 230919_Value&Adress.c

--- 230919_Value&Adress.c
+++ 230919_Value&Adress.c
@@ -11,9 +11,9 @@ int test_func() {
 	printf("%d\n", data);		// data의 저장된 값				: 5
 	printf("%p\n", &data);		// data의 주소					: 0096F9B4
 	printf("%d\n", *(&data));	// data의 주소에 담겨져 있는 값			: 5
-	printf("%p\n", ptr);		// ptr이 시작되는 ptr[0]의 주소값		: 0096F9B4
+	printf("%p\n", ptr);		// ptr이 저장하고 있는 주소값			: 0096F9B4 
 	printf("%d\n", *ptr);		// ptr이 가리키는 주소에 담겨져 있는 값 		: 5
-	printf("%p\n", &ptr);		// 배열 ptr 자체의 주소값			: 0096F9A8
+	printf("%p\n", &ptr);		// 포인터 변수 ptr 의 주소값 			: 0096F9A8
 }
 
 int main() {
