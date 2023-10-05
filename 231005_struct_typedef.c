e3a0bac9a38b6f147ec45dcb4a71445739ad3f6d
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-05 10:29:29 +0900

    Create 231005_struct_typedef.c

---
+++ 231005_struct_typedef.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+struct PROFILE0 {
+	int age;
+	double height;
+	char name[20];
+} pf0;
+
+typedef struct PROFILE1{
+	int age;
+	double height;
+	char name[20];
+} pf1;
+
+void test_func() {
+	pf0.age = 31;
+	pf1 _pf1;
+	_pf1.age = 5;
+}
+
+int main() {
+	test_func();
+	return 0;
+}
