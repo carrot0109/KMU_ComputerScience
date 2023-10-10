eb54c1a343b7e1c813d68e8c0178312731fd7b32
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-10 14:18:27 +0900

    Create 231010_struct_ptr.c

---
+++ 231010_struct_ptr.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <assert.h>
+
+struct score {
+	int kor, eng, mat;
+};
+
+void ex_func() {
+	struct score a = { 90,80,70 };
+	struct score* sp = &a;  // 구조체 포인터 변수
+	printf("국어 : %d\n", (*sp).kor);  // 참조연산자 통해 저장된 값에 접근
+	printf("영어 : %d\n", (*sp).eng);
+	printf("수학 : %d\n", (*sp).mat);
+}
+
+int main() {
+	ex_func();
+	return 0;
+}
