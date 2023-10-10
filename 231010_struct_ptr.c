7f5797761bab1802c52a3c654e3ecdedeeaef999
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-10 14:22:58 +0900

    Update 231010_struct_ptr.c

--- 231010_struct_ptr.c
+++ 231010_struct_ptr.c
@@ -10,9 +10,14 @@ struct score {
 };
 
 void ex_func() {
-	struct score a = { 90,80,70 };
+	struct score a = { 90,80,70 };	// 구조체 변수 a
 	struct score* sp = &a;  // 구조체 포인터 변수
-	printf("국어 : %d\n", (*sp).kor);  // 참조연산자 통해 저장된 값에 접근
+	printf("국어 : %d\n", (*sp).kor);  
+	
+	// 멤버 참조연산자 "." 을 이용하여 멤버변수에 접근
+	// 참조연산자를 이용하여 주소에 해당하는 구조체변수에 접근
+	// "." 이 "*" 보다 우선순위가 높음!! --> 괄호 필수
+	
 	printf("영어 : %d\n", (*sp).eng);
 	printf("수학 : %d\n", (*sp).mat);
 }
