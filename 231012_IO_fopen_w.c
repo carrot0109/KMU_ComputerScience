50f5ebff74a634dd4432d9737370d4186ed583e1
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-12 15:04:24 +0900

    Update 231012_IO_fopen_w.c

--- 231012_IO_fopen_w.c
+++ 231012_IO_fopen_w.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 void ex_func() {
-    FILE* fp = NULL;
+    FILE* fp = NULL;		// stream 생성
     fp = fopen("data.txt","w");
-    fprintf(fp,"손흥민 202301 축구 좋아하는 숫자 %d", 7);  // 메모장 생성과 문장 메모장에 입력
+    fprintf(fp,"손흥민 202301 축구 좋아하는 숫자 %d", 7);  // 파일 입출력 함수 --> 메모장 생성과 문장 메모장에 입력
     // 내용을 바꿀 시 전에 있던 파일이 날아감
 }
 
