e81667e915adbc89c50cc79169c1f1b870e129d4
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-10-17 15:29:11 +0900

    Create 231017_IO_binary.c

---
+++ 231017_IO_binary.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void test_func() {
+	FILE* fp1 = fopen("data1.txt", "wt");
+	FILE* fp2 = fopen("data2.txt", "wb");
+
+	if (fp1 == NULL) {
+		printf("file open error\n");
+		return;
+	}
+
+	if (fp2 == NULL) {
+		printf("file open error\n");
+		return;
+	}
+
+	int num = 65;
+	fprintf(fp1, "%d", num);
+	fwrite(&num, sizeof(num), 1, fp2);
+	fclose(fp1);	fclose(fp2);
+
+
+	fp1 = fopen("data1.txt", "rt");
+	fp2 = fopen("data2.txt", "rb");
+
+	if (fp1 == NULL) {
+		printf("file open error\n");
+		return;
+	}
+
+	if (fp2 == NULL) {
+		printf("file open error\n");
+		return;
+	}
+
+	num = 0;
+	fscanf(fp1, "%d", &num);
+	fwrite(&num, sizeof(num), 1, fp2);
+	printf("%d\n", num);
+
+	char str; 
+	num = 0;
+	fread(&str, sizeof(str), 1, fp2);
+	printf("%c\n", str);
+
+	fclose(fp1);	fclose(fp2);
+}
+
+
+int main() {
+	test_func();
+	return;
+}
