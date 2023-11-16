99c0e170409507bbbdb8ba8f791e58f77c231b4f
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2023-11-16 09:29:45 +0900

    Update 231114_stack_push&pop.c

--- 231114_stack_push&pop.c
+++ 231114_stack_push&pop.c
@@ -1,100 +1,134 @@
-#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <memory.h>
-#include <assert.h>
 
-#define STACK_SIZE 10
+#define STACK_SIZE	10
 
 typedef int element;
 
 element stack[STACK_SIZE];
 int top = -1;
 
-void init() {
+void init()
+{
 	top = -1;
 }
 
-int isEmpty() {
+int inEmpty()
+{
 	if (top == -1)
-		return -1;
+	{
+		return 1;		// TRUE
+	}
 	else
-		return 0;
+	{
+		return 0;		// FALSE
+	}
 }
 
-int isFull() {
-	if (top == STACK_SIZE - 1)
+int isFull()
+{
+	if (top == STACK_SIZE - 1) {
 		return 1;
-	else 
+	}
+	else
+	{
 		return 0;
+	}
 }
 
-void push(element item) {
-	if (isFull()) {
-		printf("\n\n Stack is FULL \n");
+void push(element item)
+{
+	if (isFull())
+	{
+		printf("\n\tStack is Full!\n\n");
 		return;
 	}
 	else
-		stack[++top] = item;
-}
-
-element pop() {
-	if (isEmpty()) {
-		printf("\n\n Stack is Empty \n");
-		return 0;
+	{
+		stack[++top] = item;		// top의 값을 증가시킨 후 현재 top에 원소 삽입
 	}
-	else
-		return stack[top--];		// top의 원소를 삭제한 후 top 감소
 }
 
-element peek() {
-	if (isEmpty()) {
-		printf("\n\n Stack is Empty\n");
+element pop()
+{
+	if (inEmpty())
+	{
+		printf("\n\tStack is Empty!\n\n");
 		exit(1);
 	}
 	else
-		return stack[top];
-}
-
-void printStack() {
-	for (int i = 0;i <= top;i++) {
-		printf("\n[%d]번째 스택값은 [%d]입니다.",i, stack[i]);
+	{
+		return stack[top--];		// 현재 top위치의 데이터를 반환한 후에, top의 값을 감소
 	}
 }
 
-void exer1_stack_array() {
+element peek()
+{
+	if (inEmpty())
+	{
+		printf("\n\tStack is Empty!\n\n");
+		exit(1);
+	}
+	else
+	{
+		return stack[top];		// 현재 top위치의 데이터를 반환한 후에도, top의 값을 유지
+	}
+}
+
+void printStack()
+{
+	int cnt_i;
+
+	if (inEmpty())
+	{
+		printf("\n스택이 비어있습니다.\n");
+		return;
+	}
+	printf("[");
+	for (cnt_i = 0; cnt_i <= top; cnt_i++)
+	{
+		printf("%d   ", stack[cnt_i]);
+	}
+	printf("]\n");
+}
+
+void test1()
+{
+	int cnt_i;
 	element item;
-	printf("\n** 순차 스택 연산 **\n");
-	printStack();
-	int i = 0;
 
-	for (i = 0;i < 10;i++) {
-		push(i);
+	printStack();
+
+	for (cnt_i = 0; cnt_i < 10; cnt_i++)
+	{
+		push(cnt_i);
+		printf("push ");
+		printStack();
+	}
+	//getchar();
+
+	/* overflow test */
+	push(cnt_i);
+	//getchar();
+
+	for (cnt_i = 0; cnt_i < 10; cnt_i++)
+	{
+		printf("pop ");
+		printf("%d ", pop());
 		printStack();
 	}
 
-	push(i);
-	printStack();
-
-	item = peek();
-	printStack();
-	printf("peek => %d", item);
-
-	for (i = 0;i < 10;i++) {
-		item = pop();
-		printStack();
-		printf("\t pop => %d", item);
-	}
-
-	item = pop();
-	printStack();
-
+	/* underflow test */
+	printf("%d ", pop());
 	getchar();
 }
 
-int main() {
-	exer1_stack_array();
+int main()
+{
+	test1();
+	getchar();
 	return 0;
 }
+
+// EOF
